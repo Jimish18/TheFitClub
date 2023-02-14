1d3c9959c462504267f0dee2238cb0009cda6329
jimish1806 <jimish957@gmail.com>
2023-02-14 23:39:45 +0530

    creating "Plans" Section

---
+++ src/components/Plans/Plans.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Plans.css'
+import {plansData} from '../../data/plansData'
+
+const Plans = () => {
+  return (
+    <div className="Plans" id="plans">
+
+        <div className="programs-header">
+            <span className='stroke-text'>READY TO START</span>
+            <span>YOUR JOURNEY</span>
+            <span className='stroke-text'>NOW WITHUS</span>
+        </div>
+
+        {/* Plans Cards */}
+        <div className="plans-card">
+
+        </div>
+
+    </div>
+  )
+}
+
+export default Plans;
