1a95d2bf1151db611c20451a773bbc9bd68bc8c0
jimish1806 <jimish957@gmail.com>
2023-02-15 03:39:23 +0530

    creating "Join" Section

---
+++ src/components/Join/Join.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Join.css'
+
+const Join = () => {
+  return (
+    <div className='Join' id='join-us'>
+
+        <div className="left-j">
+
+            <hr />
+            <div>
+                <span className='stroke-text'>ready to </span>
+                <span>level up</span>
+            </div>
+
+            <div>
+                <span>your body </span>
+                <span className='stroke-text'>with us ?</span>
+            </div>
+
+        </div>
+        <div className="right-j"></div>
+    </div>
+  )
+}
+
+export default Join;
